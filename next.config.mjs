df9a0b377c5ba981f3fbe15a49aebf2b5c16be1c
Raye Rayne <247046349+21leahcimhtiek-oss@users.noreply.github.com>
2026-04-04 16:06:43 -0700

    Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,43 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@sentry/nextjs"],
+  },
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "s.gravatar.com",
+      "cdn.discordapp.com",
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+const sentryWebpackPluginOptions = {
+  org: "aurora-rayes",
+  project: "incidentiq",
+  silent: true,
+  widenClientFileUpload: true,
+  hideSourceMaps: true,
+  disableLogger: true,
+};
+
+export default withSentryConfig(nextConfig, sentryWebpackPluginOptions);
